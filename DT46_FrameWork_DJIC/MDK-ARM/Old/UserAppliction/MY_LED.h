639c67556b56c09e72c7260d03b8aa0b21b2f518
Zhangziji-Cumulus <3363997071@qq,com>
2026-07-21 10:14:46 +0800

    删除残留旧文件

--- DT46_FrameWork_DJIC/MDK-ARM/Old/UserAppliction/MY_LED.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MY_LED_H
-#define __MY_LED_H
-
-
-#include <stdint.h>
-
-uint8_t LED_Flash(uint16_t FlashTime,uint16_t TimeUint);
-
-#endif
--- DT46_FrameWork_DJIC/MDK-ARM/Old/UserAppliction/UAP_Init.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UAP_INIT_H
-#define __UAP_INIT_H
-
-#include "main.h"
-
-
-void UAP_Init(void);
-
-#endif
